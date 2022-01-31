06e5c93098663016b8256f4fdfe79cdf9a41c0f9
gregsleg <gregorjery@gmail.com>
2022-01-31 17:42:01 +0000

    update cubefall
    made good change 4 cpu power

--- Scripts/cubefall.gd
+++ Scripts/cubefall.gd
@@ -4,6 +4,7 @@ extends Spatial
 # Declare member variables here. Examples:
 var speed = 2
 onready var hitbox = $CSGCombiner/CSGBox/Area
+var stopped=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +13,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	translation -= Vector3(0, speed*delta, 0)
-	if len(hitbox.get_overlapping_areas()) > 0:
+	if len(hitbox.get_overlapping_areas()) > 0 and stopped == false:
 		speed = 0
+		stopped=true
 
