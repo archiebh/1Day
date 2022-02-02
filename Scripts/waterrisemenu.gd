ed5d0ad30e6176a2a12046f726d1bf0cccae695b
archiebh <archiehutchison68@gmail.com>
2022-02-02 15:52:00 +0000

    menu first

---
+++ Scripts/waterrisemenu.gd
@@ -0,0 +1,16 @@
+extends CSGBox
+
+
+var speed = 1.5
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translation = Vector3(0, 2*delta, 0) + translation
+	pass
