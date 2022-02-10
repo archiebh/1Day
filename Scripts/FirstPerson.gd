5c6869f13b74fbed880872dde72c652bf287b5f1
archiebh <archiehutchison68@gmail.com>
2022-02-10 16:37:51 +0000

    bhop

--- Scripts/FirstPerson.gd
+++ Scripts/FirstPerson.gd
@@ -189,7 +189,7 @@ func _physics_process(delta):
 		accel = ACCEL_AIR
 		gravity_vec += Vector3.DOWN * gravity * delta
 		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		snap = Vector3.ZERO
 		gravity_vec = Vector3.UP * jump
 		walljump = 1
